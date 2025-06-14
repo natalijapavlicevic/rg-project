83b241d5abcd14fc16be9c3bc64badfd73c72241
natalijapavlicevic <pavlicevicnatalija@gmail.com>
2025-06-14 21:14:13 +0200

    Set up MainController: draw and camera

--- app/include/MainController.hpp
+++ app/include/MainController.hpp
@@ -12,10 +12,21 @@ class MainController : public engine::core::Controller {
     void initialize() override;
     bool loop() override;
 
+
 public:
     std::string_view name() const override {
-        return "MainController";
+        return "app::MainController";
     }
+
+private:
+    // void draw_skybox();
+    void draw_backpack();
+    void begin_draw() override;
+    void draw_skybox();
+    void draw() override;
+    void end_draw() override;
+    void update_camera();
+    void update() override;
 };
 
 }// namespace app
--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -2,13 +2,25 @@
 // Created by natalija on 3/1/25.
 //
 
-#include "../include/MainController.h"
+#include "../include/MainController.hpp"
 
+#include "GUIController.hpp"
+#include "engine/graphics/OpenGL.hpp"
+#include "spdlog/spdlog.h"
+
+#include <engine/graphics/GraphicsController.hpp>
 #include <engine/platform/PlatformController.hpp>
+#include <engine/resources/ResourcesController.hpp>
+
 
 namespace app {
 void MainController::initialize() {
+    // spdlog::info("Main controller initialized");
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    platform->register_platform_event_observer(std::make_unique<MainPlatformEventObserver>());
+    engine::graphics::OpenGL::enable_depth_testing();
 }
+
 bool MainController::loop() {
     auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
     if (platform->key(engine::platform::KEY_ESCAPE).is_down()) {
@@ -16,4 +28,77 @@ bool MainController::loop() {
     }
     return true;
 }
+
+void MainController::draw_backpack() {
+    auto resources = engine::core::Controller::get<engine::resources::ResourcesController>();
+    auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+    engine::resources::Model *backpack = resources->model("backpack");
+    engine::resources::Shader *shader = resources->shader("basic");
+
+    shader->use();
+    shader->set_mat4("projection", graphics->projection_matrix());
+    shader->set_mat4("view", graphics->camera()->view_matrix());
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
+    model = glm::scale(model, glm::vec3(0.3f));
+    shader->set_mat4("model", model);
+    backpack->draw(shader);
+}
+void MainController::begin_draw() {
+    engine::graphics::OpenGL::clear_buffers();
+}
+
+void MainController::draw_skybox() {
+    auto resources = engine::core::Controller::get<engine::resources::ResourcesController>();
+    auto skybox = resources->skybox("nature_skybox");
+    auto shader = resources->shader("skybox");
+    auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+    graphics->draw_skybox(shader, skybox);
+}
+
+void MainController::draw() {
+    draw_backpack();
+    draw_skybox();
+}
+
+void MainController::end_draw() {
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    platform->swap_buffers();
+}
+
+void MainController::update_camera() {
+    auto gui_controller = engine::core::Controller::get<GUIController>();
+    if (gui_controller->is_enabled()) {
+        return;
+    }
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+    auto camera = graphics->camera();
+    float dt = platform->dt();
+
+    if (platform->key(engine::platform::KEY_W).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::FORWARD, dt);
+    }
+
+    if (platform->key(engine::platform::KEY_S).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::BACKWARD, dt);
+    }
+
+    if (platform->key(engine::platform::KEY_A).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::LEFT, dt);
+    }
+
+    if (platform->key(engine::platform::KEY_D).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::RIGHT, dt);
+    }
+
+    // camera->update(platform->time());
+}
+
+void MainController::update() {
+    update_camera();
+}
+
+
+
 }// namespace app

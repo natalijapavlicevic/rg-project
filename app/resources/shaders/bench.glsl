59b4775d9e1831b255ac48ec3968950623bf2a61
natalijapavlicevic <pavlicevicnatalija@gmail.com>
2025-10-08 16:56:29 +0200

    Change a detail in bench shader

--- app/resources/shaders/bench.glsl
+++ app/resources/shaders/bench.glsl
@@ -35,7 +35,7 @@ struct Material {
     float shininess;
 };
 
-struct Light {
+struct SpotLight {
     vec3 position;
     vec3 direction;
     float cutOff;
@@ -52,31 +52,31 @@ struct Light {
 
 uniform vec3 viewPos;
 uniform Material material;
-uniform Light light;
+uniform SpotLight spotlight;
 uniform vec3 lightColor;
 
 void main()
 {
-    vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
+    vec3 ambient = spotlight.ambient * texture(material.diffuse, TexCoords).rgb;
 
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(light.position - FragPos);
+    vec3 lightDir = normalize(spotlight.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
+    vec3 diffuse = spotlight.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
 
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
+    vec3 specular = spotlight.specular * spec * texture(material.specular, TexCoords).rgb;
 
-    float theta = dot(lightDir, normalize(-light.direction));
-    float epsilon = (light.cutOff - light.outerCutOff);
-    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+    float theta = dot(lightDir, normalize(-spotlight.direction));
+    float epsilon = (spotlight.cutOff - spotlight.outerCutOff);
+    float intensity = clamp((theta - spotlight.outerCutOff) / epsilon, 0.0, 1.0);
     diffuse *= intensity;
     specular *= intensity;
 
-    float distance = length(light.position - FragPos);
-    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+    float distance = length(spotlight.position - FragPos);
+    float attenuation = 1.0 / (spotlight.constant + spotlight.linear * distance + spotlight.quadratic * (distance * distance));
     ambient *= attenuation;
     diffuse *= attenuation;
     specular *= attenuation;

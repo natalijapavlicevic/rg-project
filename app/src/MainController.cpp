41745d1d0dfa996c8bf6a8ffcc563d8de704ff68
natalijapavlicevic <pavlicevicnatalija@gmail.com>
2025-10-09 16:12:46 +0200

    Fix specular light for grass

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -125,7 +125,7 @@ void MainController::draw_grass() {
 
     shader->set_vec3("spotlight.ambient", glm::vec3(0.1f));
     shader->set_vec3("spotlight.diffuse", light_color * 0.8f);
-    shader->set_vec3("spotlight.specular", light_color);
+    shader->set_vec3("spotlight.specular", glm::vec3(0.0f));
 
     shader->set_float("spotlight.constant", 1.0f);
     shader->set_float("spotlight.linear", 0.05f);
@@ -133,13 +133,13 @@ void MainController::draw_grass() {
     shader->set_vec3("lightColor", light_color);
     shader->set_vec3("viewPos", camera->Position);
     shader->set_int("material.diffuse", 0);
-    shader->set_int("material.specular", 1);
-    shader->set_float("material.shininess", 32.0f);
+    shader->set_int("material.specular", 0);
+    shader->set_float("material.shininess", 5.0f);
 
     shader->set_vec3("dirlight.direction", camera->Front);
     shader->set_vec3("dirlight.ambient", glm::vec3(0.1f));
-    shader->set_vec3("dirlight.diffuse", glm::vec3(0.3f));
-    shader->set_vec3("dirlight.specular", glm::vec3(0.2f));
+    shader->set_vec3("dirlight.diffuse", glm::vec3(0.1f));
+    shader->set_vec3("dirlight.specular", glm::vec3(0.0f));
 
     grass->draw(shader);
 }
